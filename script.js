af30a612f61e4387e70ab6e759405da09874279a
Lucifer-mk <manojkaprimanoj@gmail.com>
2025-01-05 11:56:17 +1100

    new and improved generator

--- script.js
+++ script.js
@@ -1,30 +1,16 @@
-function generatePassword() {
-    var length = document.getElementById("length").value;
-    var includeUppercase = document.getElementById("includeUppercase").checked;
-    var includeLowercase = document.getElementById("includeLowercase").checked;
-    var includeNumbers = document.getElementById("includeNumbers").checked;
-    var includeSymbols = document.getElementById("includeSymbols").checked;
-    var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-    var numberChars = "0123456789";
-    var symbolChars = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
-    var validChars = "";
-    if (includeUppercase) {
-        validChars += uppercaseChars;
+function generate(){
+    let password = '';
+    let cheractorset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
+    for (let i = 0; i<12; i++){
+        let passwordIndex = Math.floor(Math.random()*cheractorset.length);
+        password += cheractorset[passwordIndex];
     }
-    if (includeLowercase) {
-        validChars += lowercaseChars;
-    }
-    if (includeNumbers) {
-        validChars += numberChars;
-    }
-    if (includeSymbols) {
-        validChars += symbolChars;
-    }
-    var password = "";
-    for (var i = 0; i < length; i++) {
-        var randomIndex = Math.floor(Math.random() * validChars.length);
-        password += validChars.charAt(randomIndex);
-    }
-    document.getElementById("output").value = password;
+    document.getElementById('password').value = password;
+}
+function copy(){
+    let copyText = document.getElementById('password');
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    alert('Password copied to clipboard');
 }
